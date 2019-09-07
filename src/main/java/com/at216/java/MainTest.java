0fc981ebae0e88801cf0c668e9035eb16a4b7ee0
zzbb425 <1047441153@qq.com>
2019-09-07 20:11:10 +0800

    输出10个整数

--- src/main/java/com/at216/java/MainTest.java
+++ src/main/java/com/at216/java/MainTest.java
@@ -3,5 +3,9 @@ package com.at216.java;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+        }
     }
 }
